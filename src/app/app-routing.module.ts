79802f7f21a4a1fc9f0c00eb4995c90aa1f17e4e
abelayala41 <abelayala41@gmail.com>
2021-05-21 05:03:27 -0500

    Se agrego comidas y cena

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'inicio',
+    pathMatch:'full'
+  },
+
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'crear-cuenta',
+    loadChildren: () => import('./crear-cuenta/crear-cuenta.module').then( m => m.CrearCuentaPageModule)
+  },
+  {
+    path: 'comidas',
+    loadChildren: () => import('./comidas/comidas.module').then( m => m.ComidasPageModule)
+  },
+  {
+    path: 'cena',
+    loadChildren: () => import('./cena/cena.module').then( m => m.CenaPageModule)
+  }
+
+
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
